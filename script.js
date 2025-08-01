4cccb99e2c86c7a485c98f1eb436c7c5fb67c3dc
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-08-01 21:42:03 +0200

    removing event listners from the main function

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-document.addEventListener("DOMContentLoaded", (e) => {});
-const addButton = document.getElementById("add-task-btn");
-const taskInput = document.getElementById("task-input");
-const taskList = document.getElementById("task-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
 
-function addTask() {
-  const taskText = taskInput.value.trim();
-  if (taskText === "") {
-    alert("Please enter a task!");
-  } else {
-    const task = document.createElement("li");
-    task.textContent = taskText;
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove-btn");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", () => {
-      task.remove();
-    });
-    task.appendChild(removeButton);
-    taskList.appendChild(task);
-    taskInput.value = "";
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("Please enter a task!");
+    } else {
+      const task = document.createElement("li");
+      task.textContent = taskText;
+      const removeButton = document.createElement("button");
+      removeButton.classList.add("remove-btn");
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", () => {
+        task.remove();
+      });
+      task.appendChild(removeButton);
+      taskList.appendChild(task);
+      taskInput.value = "";
+    }
   }
   addButton.addEventListener("click", addTask);
-  taskInput.addEventListener("keypress", addTask);
-}
+  taskInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTask();
+    }
+  });
+});
 
-addTask();
+// addTask();
